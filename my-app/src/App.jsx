eab263c84b6cb9980de70b5c428e7aa8817e36cb
Boburbek <boburbek.utkirov10@gmail.com>
2022-04-12 21:17:16 +0500

    react project one

---
+++ my-app/src/App.jsx
@@ -0,0 +1,24 @@
+import Cards from "./components/Cards"
+import Container from "./Container";
+import Footer from "./Footer";
+
+import Navbar from "./Navbar";
+
+
+export default function App() {
+
+  return (
+
+    <div id="container">
+      <Navbar />
+
+      <Cards />
+
+      <Footer />
+      <Container />
+
+    </div>
+
+
+  )
+}
---
+++ my-app/src/components/Cards.jsx
@@ -0,0 +1,19 @@
+export default function Cards(props) {
+    console.log(props.name);
+    return (
+
+        <div className="card">
+
+            <h1>It’s a Big World <br />
+                Out There, Go <br />
+                Explore</h1>
+            <ol>Conveniently customize proactive web services for leveraged without <br />
+                continualliery aggregate fricctionle ou wellies richard.and very <br />
+                customize  continually.</ol>
+
+            <button className="btn2">Get Exploration </button>
+            <button className="btn3">Read More</button>
+
+        </div>
+    )
+}
